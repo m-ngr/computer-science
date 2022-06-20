12bf951885cbb87299047a77050261a21b6a4bc8
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-06-20 22:20:11 +0200

    section 5 completed

---
+++ courses/programming-languages/part-b/section-5/hw/solution/hw4.rkt
@@ -0,0 +1,132 @@
+;; Mahmoud Elnagar 20/06/2022
+#lang racket
+(provide (all-defined-out))
+
+;======================================= Problem 1 =============================================
+;; Number Number Number -> ListOfNumber
+;; produce a list of numbers from low to high (including low and possibly high)
+;; separated by stride and in sorted order. Assume stride is positive.
+
+(define (sequence low high stride)
+  (if (> low high)
+      null
+      (cons low (sequence (+ low stride) high stride))))
+
+;======================================= Problem 2 =============================================
+;; ListOfString String -> ListOfString
+;; append the suffix to the end of all strings in the given list
+
+(define (string-append-map xs suffix)
+  (map (lambda (x) (string-append x suffix)) xs))
+
+;======================================= Problem 3 =============================================
+;; ListOfT1 Number -> T1
+;; return the i-th element in the list, where i is the remainder of (n / xs.length).
+;; raise error if n < 0 or the list is empty.
+
+(define (list-nth-mod xs n)
+  (let ([len (length xs)])
+    (cond [(< n 0) (error "list-nth-mod: negative number")]
+          [(= len 0) (error "list-nth-mod: empty list")]
+          [else (car (list-tail xs (remainder n len)))])))
+
+;======================================= Problem 4 =============================================
+;; StreamOfT1 Number -> ListOfT1
+;; produce a list of the first n values produced by the stream s in order. n is non-negative.
+
+(define (stream-for-n-steps s n)
+  (if (= n 0)
+      null
+      (let ([pr (s)])
+        (cons (car pr)(stream-for-n-steps (cdr pr) (- n 1))))))
+
+;======================================= Problem 5 =============================================
+;; funny-number-stream is a thunk of (cons Number funny-number-stream)
+;; a stream of natural numbers except numbers divisble by 5 are negated
+
+(define (funny-number-stream)
+  (letrec ([gen (lambda (x)
+                  (cons (if (= (remainder x 5) 0) (- x) x)
+                        (lambda ()(gen (+ x 1)))))])
+    (gen 1)))
+
+;======================================= Problem 6 =============================================
+;; dan-then-dog is a thunk of (cons String dan-then-dog)
+;; a stream that alternates between the strings "dan.jpg" and "dog.jpg"
+
+(define (dan-then-dog)
+  (letrec ([gen (lambda (x)
+                  (cons (if x "dan.jpg" "dog.jpg")
+                        (lambda () (gen (not x)))))])
+    (gen true)))
+
+;======================================= Problem 7 =============================================
+;; StreamOfT1 -> StreamOf(Number.T1)
+;; produces a stream of pair (0 . v) for each v in the given stream
+;; where v is the element produced by the given stream
+
+(define (stream-add-zero s)
+  (lambda ()
+    (let ([pr (s)])
+      (cons (cons 0 (car pr))
+            (stream-add-zero (cdr pr))))))
+
+;======================================= Problem 8 =============================================
+;; ListOfT1 ListOfT2 -> StreamOf(T1.T2)
+;; Produces a stream of pair (v1.v2). The stream cycles forever through the lists.
+;; Assume: Lists are non-empty, but may or may not be the same length.
+
+(define (cycle-lists xs ys)
+  (letrec ([loop (lambda (n)
+                (cons (cons (list-nth-mod xs n)
+                            (list-nth-mod ys n))
+                      (lambda ()(loop (+ n 1)))))])
+    (lambda ()(loop 0))))
+
+;======================================= Problem 9 =============================================
+;; T1 VectorOfAny -> (T1.T2) or #f
+;; produces the first pair in the given vector that starts with v. #f if no such pair is found
+;; skip all non-pair elements
+
+(define (vector-assoc v vec)
+  (letrec ([search (lambda (n)
+                     (if (>= n (vector-length vec))
+                         #f
+                         (let ([vpr (vector-ref vec n)])
+                           (if (and (pair? vpr) (equal? (car vpr) v))
+                               vpr
+                               (search (+ n 1))))))])
+    (search 0)))
+
+;======================================= Problem 10 ============================================
+;; ListOf(T1.T2) Number -> ( T1 -> (T1.T2) or #f )
+;; returns a function that takes one argument v and returns the same thing as (assoc v xs)
+;; using internal cache (a vector of n elements) to store the most recent n calls.
+;; The cache starts empty (a vector of n elements all have value #f).
+;; The cache slots are used in a round-robin fashion: from 0 to n-1 and back to 0 again.
+
+(define (cached-assoc xs n)
+  (let ([cache (make-vector n #f)]
+        [index 0])
+    (lambda (v)
+      (let ([cache-ans (vector-assoc v cache)])
+        (if cache-ans
+            (cdr cache-ans)
+            (let ([ans (assoc v xs)])
+              (begin (vector-set! cache index (cons v ans))
+                     (set! index (remainder (+ index 1) n))
+                     ans)))))))
+
+;======================================= Problem 11 ============================================
+;; MACRO: (while-less e1 do e2)
+
+(define-syntax while-less
+  (syntax-rules (do)
+    [(while-less e1 do e2)
+     (letrec ([v1 e1]
+              [loop (lambda ()
+                      (if (< e2 v1)
+                          (loop)
+                          #t))])
+       (loop))]))
+
---
+++ courses/programming-languages/part-b/section-5/hw/solution/hw4.test.rkt
@@ -0,0 +1,129 @@
+;; Mahmoud Elnagar 20/06/2022
+#lang racket
+(require "hw4.rkt")
+(require rackunit)
+(require rackunit/text-ui)
+
+;; Helper functions
+(define ones (lambda () (cons 1 ones)))
+(define (make-stream f v) (lambda() (cons v (make-stream f (f v)))))
+(define nats (make-stream (lambda (x) (+ x 1)) 1))
+
+;; Tests
+(run-tests
+ (test-suite
+  "Problem 1: sequence"
+  (check-equal? (sequence 3 2 1) '() "low > high")
+  (check-equal? (sequence 3 3 1) (list 3) "low = high")
+  (check-equal? (sequence 1 9 50) (list 1) "Large Stride")
+  (check-equal? (sequence 0 5 1) (list 0 1 2 3 4 5) "Small Stride")
+  (check-equal? (sequence 3 11 2) (list 3 5 7 9 11) "high included")
+  (check-equal? (sequence 3 8 3) (list 3 6) "high excluded")
+  ))
+
+(run-tests
+ (test-suite
+  "Problem 2: string-append-map"
+  (check-equal? (string-append-map '() ".ngr") '() "Empty List")
+  (check-equal? (string-append-map '("m") "-ngr") '("m-ngr") "1 element List")
+  (check-equal? (string-append-map 
+                 (list "dan" "dog" "curry" "dog2")
+                 ".jpg") '("dan.jpg" "dog.jpg" "curry.jpg" "dog2.jpg") "many elements")
+  ))
+           
+(run-tests
+ (test-suite
+  "Problem 3: list-nth-mod"
+  (check-exn exn:fail? (lambda() (list-nth-mod (list 0 1 2 3 4) -2)) "negative n throws")
+  (check-exn exn:fail? (lambda() (list-nth-mod '() 2)) "empty xs throws")
+  (check-equal? (list-nth-mod (list "a" "b") 0) "a" "n = 0")
+  (check-equal? (list-nth-mod (list 0 1 2 3 4) 2) 2 "n < xs.length")
+  (check-equal? (list-nth-mod (list 0 1 2 3 4) 5) 0 "n = xs.length")
+  (check-equal? (list-nth-mod (list 0 1 2 3 4) 7) 2 "n > xs.length")
+  ))
+
+(run-tests
+ (test-suite
+  "Problem 4: stream-for-n-steps"
+  (check-equal? (stream-for-n-steps nats 0) '() "no values")
+  (check-equal? (stream-for-n-steps nats 1) (list 1) "get one value from stream")
+  (check-equal? (stream-for-n-steps nats 2) (list 1 2) "get two value from stream")
+  ))
+
+(run-tests
+ (test-suite
+  "Problem 5: funny-number-stream"
+  (check-equal? (stream-for-n-steps funny-number-stream 16)
+                (list 1 2 3 4 -5 6 7 8 9 -10 11 12 13 14 -15 16)
+                "funny-number-stream test")
+  ))
+
+(run-tests
+ (test-suite
+  "Problem 6: dan-then-dog"
+  (check-equal? (stream-for-n-steps dan-then-dog 5)
+                (list "dan.jpg" "dog.jpg" "dan.jpg" "dog.jpg" "dan.jpg")
+                "dan-then-dog test")
+  ))
+
+(run-tests
+ (test-suite
+  "Problem 7: stream-add-zero"
+  (check-equal? (stream-for-n-steps (stream-add-zero nats) 3)
+                (list (cons 0 1) (cons 0 2) (cons 0 3))
+                "stream-add-zero test")
+  ))
+
+(run-tests
+ (test-suite
+  "Problem 8: cycle-lists"
+  (check-equal? (stream-for-n-steps (cycle-lists (list 1) (list "a")) 3)
+                (list (cons 1 "a") (cons 1 "a") (cons 1 "a")) 
+                "basic test")
+  
+  (check-equal? (stream-for-n-steps (cycle-lists (list 1 2 3) (list "a" "b")) 4)
+                (list (cons 1 "a") (cons 2 "b") (cons 3 "a") (cons 1 "b")) 
+                "cycle-lists test")
+  ))
+
+(run-tests
+ (test-suite
+  "Problem 9: vector-assoc"
+  (check-equal? (vector-assoc 1 (vector (cons 2 1) (cons 3 1) (cons 4 1) (cons 5 1)))
+                #f "no pair matches")
+  (check-equal? (vector-assoc 4 (vector (cons 2 1) (cons 3 1) (cons 4 1) (cons 5 1)))
+                (cons 4 1) "one pair matches")
+  (check-equal? (vector-assoc 3 (vector (cons 2 1) (cons 3 1) (cons 3 2) (cons 5 1)))
+                (cons 3 1) "two pairs matches: get the first")
+  ))
+
+(define assoc-1 (cached-assoc (list (cons 1 2) (cons 3 4) (cons 3 5)) 2))
+
+(run-tests
+ (test-suite
+  "Problem 10: cached-assoc"
+  (check-equal? (assoc-1 2) #f "no pair matches - not cached") 
+  (check-equal? (assoc-1 2) #f "no pair matches - from cache") ;((2.#f) #f)
+  (check-equal? (assoc-1 1) (cons 1 2) "one pair matches - not cached")
+  (check-equal? (assoc-1 1) (cons 1 2) "one pair matches - from cache") ;((2.#f) (1.(1.2)))
+  (check-equal? (assoc-1 3) (cons 3 4) "two pairs matches: get the first - not cached")
+  (check-equal? (assoc-1 3) (cons 3 4) "two pairs matches: get the first - from cache") ;((3.(3.4)) (1.(1.2)))
+  (check-equal? (assoc-1 1) (cons 1 2) "one pair matches - from cache") ;((3.(3.4)) (1.(1.2)))
+  (check-equal? (assoc-1 2) #f "no pair matches - not cached")
+  ))
+
+(define a 2)
+
+(run-tests
+ (test-suite
+  "Problem 11: while-less"
+  (check-equal? (while-less 7 do (begin (set! a (+ a 1)) a)) #t "terminate with #t")
+  (check-equal? a 7 "a should equal 7")
+  (check-equal? (begin (while-less  2 do (begin (set! a (+ a 1)) a)) a) 8 "e1 < e2")
+  (check-equal? (begin (while-less  8 do (begin (set! a (+ a 1)) a)) a) 9 "e1 = e2")
+  (check-equal? (begin (while-less 10 do (begin (set! a (+ a 1)) a)) a)10 "e1 = e2 + 1")
+  (check-equal? (begin (while-less 15 do (begin (set! a (+ a 1)) a)) a)15 "e1 > e2")
+  ))
+
+    
+
