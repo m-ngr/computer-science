adb2a53330270dc5a811884cda5bd9088d2f5713
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2022-07-04 23:38:02 +0200

    Homework 6 Solved

--- courses/programming-languages/part-c/section-8/hw/solution/hw6assignment.rb
+++ courses/programming-languages/part-c/section-8/hw/solution/hw6assignment.rb
@@ -1,22 +1,71 @@
-# University of Washington, Programming Languages, Homework 6, hw6runner.rb
-
-# This is the only file you turn in, so do not modify the other files as
-# part of your solution.
+# Mahmoud Elnagar 04/07/2022
 
 class MyPiece < Piece
-  # The constant All_My_Pieces should be declared here
 
-  # your enhancements here
+  def self.next_piece (board)
+    MyPiece.new(All_My_Pieces.sample, board)
+  end
 
+  All_My_Pieces = All_Pieces + [
+                 rotations([[0, 0], [1, 0], [0, 1], [1, 1], [2, 1]]), # New Shape 1
+                 [[[0, 0], [-2, 0], [-1, 0], [1, 0], [2, 0]], # New Shape 2
+                 [[0, 0], [0, -2], [0, -1], [0, 1], [0, 2]]],
+                 rotations([[0, 0], [0, 1], [1, 1]]) # New Shape 3
+                ] 
 end
 
 class MyBoard < Board
-  # your enhancements here
+  def initialize (game)
+    super
+    @current_block = MyPiece.next_piece(self)
+    @cheat = false
+  end
 
+  def cheat
+    if (!@cheat && @score >= 100)
+      @score -= 100
+      @cheat = true
+    end
+  end
+
+  def next_piece
+    if @cheat
+      @current_block = MyPiece.new([[[0,0]]], self)
+      @cheat = false
+    else
+      @current_block = MyPiece.next_piece(self)
+    end
+    @current_pos = nil
+  end
+
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    (0..(locations.size-1)).each{|index| 
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
+  
 end
 
 class MyTetris < Tetris
-  # your enhancements here
 
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
+
+  def key_bindings
+    super
+    @root.bind('u', proc {@board.rotate_clockwise; @board.rotate_clockwise})
+    @root.bind('c', proc {@board.cheat}) 
+  end
 end
 
