7b55913245a360bcf48a02133dbc439dfecaa88a
Mahmoud Elnagar <m.elnagar@expressit.com.sa>
2024-03-08 19:23:44 +0300

    Movie Rating Modification SQL

---
+++ courses/stanford-databases/1-movie-rating/q10.sql
@@ -0,0 +1,3 @@
+-- Add the reviewer Roger Ebert to your database, with an rID of 209.
+
+INSERT INTO Reviewer VALUES(209, "Roger Ebert");
---
+++ courses/stanford-databases/1-movie-rating/q12.sql
@@ -0,0 +1,8 @@
+-- Remove all ratings where the movie's year is before 1970 
+-- or after 2000, and the rating is fewer than 4 stars.
+
+DELETE FROM Rating
+WHERE mID IN (
+  SELECT mID FROM Movie
+  WHERE year < 1970 OR year > 2000
+) AND stars < 4;
